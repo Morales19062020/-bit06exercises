01b924b528b6d46415b13708a003b588c9b50bb1
¨Luis <luismorales590@gmail.com>
2023-02-03 22:43:30 -0500

    Actualizacion

--- docs/js/ejercicios.js
+++ docs/js/ejercicios.js
@@ -1,5 +1,5 @@
 "use strict";
-//Ejercicio1
+//Ejercicio Saludar//
 const $exer1Form = document.getElementById('exer1Form');
 const alertPlaceholder = document.getElementById('liveAlertPlaceholder');
 
@@ -19,6 +19,40 @@ const alert = (message, type) => {
 
   alertPlaceholder.append(wrapper);
 }
+//Pon tu Edad//
+const $mayorDeEdad = document.getElementById('form2');
+const alertPlaceholder2 = document.getElementById('liveAlertPlaceholder2')
+
+$mayorDeEdad.addEventListener('submit', (e) => {
+  e.preventDefault();
+  mayordeedad();
+});
+
+
+function mayordeedad() {
+  const $edad = document.getElementById('edad').value;
+  if ($edad >= 18) {
+    alert2('Eres mayor de edad',  'success');
+} else if($edad <= 18){
+    alert2('No eres mayor de edad',  'success');
+  
+} else {
+  alert2('Debes ingresar tu edad')
+
+}
+}
+const alert2 = (message, type) => {
+    const wrapper = document.createElement('div')
+    wrapper.innerHTML = [
+      `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+      `   <div>${message}</div>`,
+      '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+      '</div>'
+    ].join('');
+  
+    alertPlaceholder2.append(wrapper)
+}
+
 //Suma de numeros//
 const $ejercicio1 = document.getElementById('ejercicio1');
 const $sumar = document.getElementById('ejercicio1')
